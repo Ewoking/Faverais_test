140afd6dd92d6b1c3581586aade024a5eeaf22dc
thomastenot <thomastenot@outlook.fr>
2021-01-27 21:38:35 +0100

    fixed background gradient, again

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var degrade = document.querySelector("#gradient-wrapper");
 
+backgroundResize();
+window.onresize = function(e){
+    console.log(e);
+    backgroundResize();
+}
 
-if(document.body.clientHeight < degrade.clientHeight){
-    console.log(document.body.clientHeight);
-    
-    console.log("choix 1");
-    degrade.style.height = "100vh";
-}else{
-    console.log(document.body.clientHeight);
+function backgroundResize(){
+    if(document.body.clientHeight < window.innerHeight){
+        degrade.style.height = "100vh";
+        console.log("choix 1");
+    }else if(document.body.clientHeight < 2000){
+        console.log(document.body.clientHeight);
+        
+        console.log("choix 2");
+        degrade.style.height = document.body.clientHeight +"px";
+    }else{
+        console.log(document.body.clientHeight);
 
-    console.log("choix 2");
-    degrade.style.height = document.body.clientHeight +"px";
+        console.log("choix 3");
+        degrade.style.height = "2000px";
+    }
 }
